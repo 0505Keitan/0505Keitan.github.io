7f10e8154893857086c43387e28badaf1104069f
Keitan <0505Keitan@gmail.com>
2018-09-23 17:59:52 +0900

    Add files via upload

---
+++ speedtest/telemetry/telemetry_settings.php
@@ -0,0 +1,23 @@
+<?php
+
+$db_type="mysql"; //Type of db: "mysql", "sqlite" or "postgresql"
+$stats_password="PASSWORD"; //password to login to stats.php. Change this!!!
+
+// Sqlite3 settings
+$Sqlite_db_file = "../../telemetry.sql";
+
+// Mysql settings
+$MySql_username="USERNAME";
+$MySql_password="PASSWORD";
+$MySql_hostname="DB_HOSTNAME";
+$MySql_databasename="DB_NAME";
+
+// Postgresql settings
+$PostgreSql_username="USERNAME";
+$PostgreSql_password="PASSWORD";
+$PostgreSql_hostname="DB_HOSTNAME";
+$PostgreSql_databasename="DB_NAME";
+
+
+//IMPORTANT: DO NOT ADD ANYTHING BELOW THIS PHP CLOSING TAG, NOT EVEN EMPTY LINES!
+?>
