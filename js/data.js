7db319291d7a011b6272e2f6837852ed6bd9082d
Keitan <0505Keitan@gmail.com>
2018-09-29 15:39:27 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-function getDelay(){
+function getDelay() {
 
 //JSONデータ
 var data = {"release": [
@@ -31,19 +31,19 @@ var data = {"release": [
 ]}
 
 //HTMLを初期化
-$("table.tbl tbody").html("");
+("table.tbl tbody").html("");
 
 //HTMLを生成
 for(var index in data.release){
-$('<tr>'+
-'<th>'+data.release[index].day+'</th>'+
-'<td class="label"><span class="' +
-data.release[index].label + '">' +
-data.release[index].category +
-'</span></td>'+
-'<td><a href="' + data.release[index].url +
-'" target="' + data.release[index].target +
-'">' + data.release[index].content + '</a></td>'+
-'</tr>').appendTo('table.tbl tbody');
+  ('<tr>'+
+   '<th>'+data.release[index].day+'</th>'+
+   '<td class="label"><span class="' +
+   data.release[index].label + '">' +
+   data.release[index].category +
+   '</span></td>'+
+   '<td><a href="' + data.release[index].url +
+   '" target="' + data.release[index].target +
+   '">' + data.release[index].content + '</a></td>'+
+   '</tr>').appendTo('table.tbl tbody');
 }
 });
