4f7b168cc70bc3f1df5fb427be3c0244ee8a0197
Keitan <0505Keitan@gmail.com>
2018-09-29 15:45:20 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,49 +1,18 @@
-function getDelay() {
-
-//JSONデータ
-var data = {"release": [
-{
-"day": "2013.07.30",
-"label": "company",
-"category": "企業情報",
-"content": "テキストテキストテキスト",
-"url": "http://www.yahoo.co.jp/",
-"target": "_blank"
-},
-
-{
-"day": "2013.07.30",
-"label": "products",
-"category": "商品情報",
-"content": "テキストテキストテキスト",
-"url": "http://www.google.co.jp/",
-"target": "_blank"
-},
-
-{
-"day": "2013.07.30",
-"label": "company",
-"category": "企業情報",
-"content": "テキストテキストテキスト",
-"url": "http://www.yahoo.co.jp/",
-"target": "_self"
-}
-]}
+$(function(){
 
 //HTMLを初期化
-("table.tbl tbody").html("");
+$("table.tbl tbody").html("");
 
 //HTMLを生成
-for(var index in data.release){
-  ('<tr>'+
-   '<th>'+data.release[index].day+'</th>'+
-   '<td class="label"><span class="' +
-   data.release[index].label + '">' +
-   data.release[index].category +
-   '</span></td>'+
-   '<td><a href="' + data.release[index].url +
-   '" target="' + data.release[index].target +
-   '">' + data.release[index].content + '</a></td>'+
-   '</tr>').appendTo('table.tbl tbody');
-}
+$.getJSON("data.json", function(data){
+$(data.release).each(function(){
+$('<tr>'+
+'<th>'+this.day+'</th>'+
+'<td class="label"><span class="' + this.label + '">' +
+this.category + '</span></td>'+
+'<td><a href="' + this.url + '" target="' +
+this.target + '">' + this.content + '</a></td>'+
+'</tr>').appendTo('table.tbl tbody');
+})
+})
 });
