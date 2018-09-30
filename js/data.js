e655e4ca9eb9c5fc74c89f3e6ffb5f7a0d59e30f
Keitan <0505Keitan@gmail.com>
2018-09-30 15:59:48 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,15 +4,16 @@ $(function(){
 $("table.tbl tbody").html("");
 
 //HTMLを生成
-$.getJSON("data.json", function(data){
-$(data.release).each(function(){
+for(var index in data.release){
 $('<tr>'+
-'<th>'+this.day+'</th>'+
-'<td class="label"><span class="' + this.label + '">' +
-this.category + '</span></td>'+
-'<td><a href="' + this.url + '" target="' +
-this.target + '">' + this.content + '</a></td>'+
+'<th>'+data.release[index].day+'</th>'+
+'<td class="label"><span class="' +
+data.release[index].label + '">' +
+data.release[index].category +
+'</span></td>'+
+'<td><a href="' + data.release[index].url +
+'" target="' + data.release[index].target +
+'">' + data.release[index].content + '</a></td>'+
 '</tr>').appendTo('table.tbl tbody');
-})
-})
+}
 });
