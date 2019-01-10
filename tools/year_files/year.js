41b947ba4d9ec137e80079297bb8a11c5c325de5
0505Keitan <0505Keitan@gmail.com>
2019-01-10 22:06:19 +0900

    なんかよくわからんファイルたちをあげとく

--- tools/year_files/year.js
+++ tools/year_files/year.js
@@ -1,3 +1,4 @@
+/*
 date = new Date();
     year = date.getFullYear();
     if((year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0) {
@@ -15,8 +16,27 @@ years = document.getElementById("years");
 	var result = Math.floor( math ) ;
 	target.innerHTML = result;
 	years.innerHTML = year;
-	document.getElementById( "prog" ).value = date_count ;
+	document.getElementById( "prog" ).value = date_count;
 }
+*/
+
+    var now = new Date();
+    var year = now.getFullYear();
+    var oshogatsu = new Date(year + "/1/1");
+    years = now.getFullYear();
+    
+    window.onload = function onLoad() {
+    	target = document.getElementById("view");
+    	years = document.getElementById("years");
+    	var yearPercent = now.getTime() - oshogatsu.getTime();
+    	var result = yearPercent / (1000*60*60*24);
+    	var results = yearPercent / (1000*60*60*24);
+        var max = 31536000000;
+        result.toFixed(2);
+        target.innerHTML = result;
+        years.innerHTML = year;
+        document.getElementById( "prog" ).value = results;
+       }
 
 function getPost() {
 	date = new Date();
@@ -25,5 +45,5 @@ function getPost() {
 	var max = 365;
 	var math = now / max * 100;
 	var result = Math.floor( math ) ;
-	window.open("https://twitter.com/share?url=https://0505Keitan.github.io/tools/year.html&hashtags=YearComplete&text=" + year + "年は" + result + "%終了しました。", "_blank", "width=450,height=500");
+	window.open("https://twitter.com/share?url=https://0505Keitan.github.io/tools/year.html&hashtags=YearComplete&text=" + year + "年は" + math + "%終了しました。", "_blank", "width=450,height=500");
 }
