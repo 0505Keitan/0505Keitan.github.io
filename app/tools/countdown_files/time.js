87a0ae83d9c19235306d0bbe79600654a89d2cb9
0505Keitan <0505Keitan@gmail.com>
2019-12-06 15:12:39 +0900

    update : カウントダウンを綺麗に

--- app/tools/countdown_files/time.js
+++ app/tools/countdown_files/time.js
@@ -3,41 +3,48 @@ setInterval('clock()',500); //0.5秒毎にclock()を実行
 let i = (n) => ('0' + n).slice(-2);
 
 function clock() {
-    document.getElementById("view_day").innerHTML = day();
-    document.getElementById("view_hour").innerHTML = hour();
-    document.getElementById("view_min").innerHTML = min();
-    document.getElementById("view_sec").innerHTML = sec();
+    document.getElementById("now_year").innerText = year();
+    document.getElementById("view_day").innerText = day();
+    document.getElementById("view_hour").innerText = hour(year()+"/12/31 23:59:59");
+    document.getElementById("view_min").innerText = min(year()+"/12/31 23:59:59");
+    document.getElementById("view_sec").innerText = sec(year()+"/12/31 23:59:59");
+}
+
+function year() {
+	var now = new Date();
+	var year = now.getFullYear();
+	return year;
 }
 
 function day() {
     let now = new Date();
-    let anyDay = new Date(2019,11,31);
+    let anyDay = new Date(year(),11,31);
     let remainDay = Math.floor((anyDay - now) / (24*60*60*1000));
     remainDay++;
-    return i(remainDay);
+    return remainDay;
 }
 
-function hour() {
+function hour(date) {
     var now = new Date();
-    var anyDate = new Date("2019/12/31 23:59:59");//カウントダウンしたい日
+    var anyDate = new Date(date);//カウントダウンしたい日
     var ms = anyDate - now;//日数を計算
     var ha = Math.floor(ms / 3600000);//時間を取得
     var haa = ha % 24;
-    return i(haa);
+    return haa;
 }
 
-function min() {
+function min(date) {
     var now = new Date();
-    var anyDate = new Date("2019/12/31 23:59:59");//カウントダウンしたい日
+    var anyDate = new Date(date);//カウントダウンしたい日
     var ms = anyDate - now;//日数を計算
     var ha = Math.floor(ms / 3600000);//時間を取得
     var ma = Math.floor((ms - ha * 3600000) / 60000);//分を取得
     return i(ma);
 }
 
-function sec() {
+function sec(date) {
     var now = new Date();
-    var anyDate = new Date("2019/12/31 23:59:59");//カウントダウンしたい日
+    var anyDate = new Date(date);//カウントダウンしたい日
     var ms = anyDate - now;//日数を計算
     var ha = Math.floor(ms / 3600000);//時間を取得
     var ma = Math.floor((ms - ha * 3600000) / 60000);//分を取得
