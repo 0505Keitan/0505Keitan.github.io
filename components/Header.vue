0e125b759cac3dd0771359637305f259c9d485a9
0505Keitan <0505Keitan@gmail.com>
2021-03-12 13:02:26 +0900

    update: About link

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
     <a href="/" class="title">0505Keitan</a>
     <nav class="nav">
       <ul class="nav-list">
-        <li><a href="https://www.notion.so/0505keitan/0505Keitan-ebc28d56ab2f4baeb896165924054214">About</a></li>
+        <li><a href="https://scrapbox.io/0505Keitan/index">About</a></li>
         <li><a href="https://portfolio.0505keitan.now.sh/">Portfolio</a></li>
       </ul>
     </nav>
