5ba98eaf3b4c9a9ab88533ec3d15709ddb9556f6
0505Keitan <0505Keitan@gmail.com>
2022-04-13 15:12:21 +0900

    update: bio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,7 +84,7 @@ export default function Home({ desc }: Props) {
           <img src='/0505Keitan_v7.jpg' />
           <p className={styles.name}>0505Keitan</p>
           <p className={styles.bio}>
-            WEB ENGINEER / MOVIE CREATOR / UNIVERSITY STUDENT
+            BACKEND ENGINEER / DIGITAL CREATOR / UNIVERSITY STUDENT
           </p>
         </section>
       </main>
